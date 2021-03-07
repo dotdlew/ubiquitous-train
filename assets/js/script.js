7b8d309e0d81a51923bd65ac77bf9c8ab663f910
Daniel Lewis <daniel.lewis@protonmail.com>
2021-03-07 13:34:29 -0700

    adding functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,34 @@
-var searchVal = document.querySelector('#city');
-var searchButton = document.querySelector('#searchButton');
+var searchForm = document.querySelector("#search-form");
+var searchVal = document.querySelector("#form-input");
+var apiKey = "5971cbc972f6e5889ab628fd56908316";
 
-function formSubmitHandler(event){
+function formSubmitHandler(event) {
     event.preventDefault();
-
+    console.log(event);
+  
     var search = searchVal.nodeValue.trim()
     if (search) {
         getSearch(search)
         searchVal.nodeValue = "";
     } else {
-        console.error(err)
+        console.log(error);
     }
-    console.log(event)
+    console.log(search)
 }
 
-searchButton.addEventListener("submit", formSubmitHandler);
+function getSearch(search) {
+    // format url
+    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=" + apiKey;
+    
+    // fetch API data
+    fetch(apiUrl)
+        .then(response => {
+            response.json()
+                .then(data => {
+                    console.log(data)
+                })
+        })
+
+}
+
+searchForm.addEventListener("submit", formSubmitHandler);
