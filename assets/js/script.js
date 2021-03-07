bad5d063795521da88bba7f105916a3ec0df26b9
Daniel Lewis <daniel.lewis@protonmail.com>
2021-03-07 16:24:03 -0700

    added display of weather-data

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,26 @@ var searchVal = document.querySelector("#form-input");
 var presetLocations = document.querySelector("#preset-buttons");
 var apiKey = "5971cbc972f6e5889ab628fd56908316";
 
+function displayWeather(weather) {
+    var weatherDataName = document.getElementById('weather-data-name');
+    var weatherDataTemp = document.getElementById('weather-data-temperature');
+    var weatherDataHumi = document.getElementById('weather-data-humidity');
+    var weatherDataSped = document.getElementById('weather-data-wind-speed');
+    var weatherDataInde = document.getElementById('weather-data-uv-index');
+    // check if api returned any weather
+    if (weather.length === 0) {
+        weatherDataContainer.textContent = "No weather found.";
+        return;
+    }
+    
+    weatherDataName.textContent = weather.name;
+    weatherDataTemp.textContent = "Temperature: " + weather.main.temp;
+    weatherDataHumi.textContent = "Humidity: " + weather.main.humidity;
+    weatherDataSped.textContent = "Wind Speed: " + weather.wind.speed;
+    weatherDataInde.textContent = "UV Index: " + weather.weather[0].main;
+    
+}
+
 function presetsClickHandler(event) {
     var preset = event.target.getAttribute("data-location");
 
@@ -19,8 +39,6 @@ function formSubmitHandler(event) {
     if (search) {
         getSearch(search)
         searchVal.value = "";
-    } else {
-        console.log(err);
     }
 }
 
@@ -31,12 +49,19 @@ function getSearch(search) {
     // fetch API data
     fetch(apiUrl)
         .then(response => {
-            response.json()
-                .then(data => {
-                    console.log(data)
-                })
+            if (response.ok) {
+                response.json()
+                    .then(data => {
+                        console.log(data)
+                        displayWeather(data)
+                    })
+            } else {
+                console.log("error: " + response.statusText)
+            }
         })
-
+        .catch(err => {
+            console.log("error: " + err.statusText)
+        })
 }
 
 searchForm.addEventListener("submit", formSubmitHandler);
